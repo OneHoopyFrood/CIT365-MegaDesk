5c1d846db1cd0fb3758c49506a35580d658448c3
Cole Panike <colepanike@gmail.com>
2018-01-27 16:34:15 -0600

    Interface added

--- MegaDesk-3-ColePanike/MegaDesk.cs
+++ MegaDesk-3-ColePanike/MegaDesk.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MegaDesk_3_ColePanike
 {
-    public partial class Form1 : Form
+    public partial class MegaDesk : Form
     {
-        public Form1()
+        public MegaDesk()
         {
             InitializeComponent();
         }
