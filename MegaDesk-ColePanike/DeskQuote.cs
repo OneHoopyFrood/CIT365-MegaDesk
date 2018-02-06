fd98e0b56ac7dfe7b7edadf2234b90011ac77c33
Cole Panike <colepanike@gmail.com>
2018-02-06 00:08:19 -0600

    Rename solution to exclude lesson number.
    This is much safer than renaming the solution EVERY time to something with the lesson number. It shouldn't affect submissions.

---
+++ MegaDesk-ColePanike/DeskQuote.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaDesk_ColePanike
+{
+    public class DeskQuote
+    {
+        public Desk Desk;
+        private string customerName;
+        public string CustomerName { get => customerName; set => customerName = value; }
+        public decimal QuoteAmount
+        {
+            get;
+            private set;
+        }
+        public DateTime QuoteDate
+        {
+            get;
+            private set;
+        }
+        public int ProductionDays
+        {
+            get;
+            private set;
+        }
+
+        public DeskQuote(string customerName, Desk desk, int productionDays)
+        {
+            this.CustomerName = customerName;
+            this.Desk = desk;
+            this.ProductionDays = productionDays;
+            
+        }
+
+        public DeskQuote(string customerName, int width, int depth, int numDrawers, Desk.DeskMaterial material, int productionDays) 
+            : this(customerName, new Desk(width, depth, numDrawers, material), productionDays)
+        {
+
+        }
+
+        private void CalculateQuote()
+        {
+            decimal cost = 200;
+            int surfaceArea = this.Desk.GetSurfaceArea();
+            if (surfaceArea > 1000)
+                cost += (surfaceArea - 1000);
+            cost += Desk.NumDrawers * 50;
+
+            switch (this.Desk.Material)
+            {
+                case Desk.DeskMaterial.Pine:
+                    cost += 50;
+                    break;
+                case Desk.DeskMaterial.Laminate:
+                    cost += 100;
+                    break;
+                case Desk.DeskMaterial.Veneer:
+                    cost += 125;
+                    break;
+                case Desk.DeskMaterial.Oak:
+                    cost += 200;
+                    break;
+                case Desk.DeskMaterial.Rosewood:
+                    cost += 300;
+                    break;
+            }
+
+            if (IsRush())
+            {
+                switch (this.ProductionDays)
+                {
+                    case 3:
+                        cost += 60;
+                        cost += (surfaceArea % 1000) * 10;
+                        break;
+                    case 5:
+                        cost += 40;
+                        cost += (surfaceArea % 1000) * 10;
+                        break;
+                    case 7:
+                        cost += 30;
+                        cost += (surfaceArea % 1000) * 5;
+                        break;
+                }
+            }
+
+            this.QuoteAmount = cost;
+        }
+
+        private bool IsRush()
+        {
+            return ProductionDays < 14;
+        }
+    }
+}
---
+++ MegaDesk-ColePanike/MegaDesk.cs
@@ -0,0 +1,43 @@
+﻿using MegaDesk_ColePanike.Resources;
+using System;
+using System.Windows.Forms;
+
+namespace MegaDesk_ColePanike
+{
+    public partial class MegaDesk : Form
+    {
+        public MegaDesk()
+        {
+            InitializeComponent();
+        }
+
+        private void addQuoteButton_Click(object sender, EventArgs e)
+        {
+            AddQuote addNewQuoteForm = new AddQuote();
+            addNewQuoteForm.Tag = this;
+            addNewQuoteForm.Show(this);
+            Hide();
+        }
+
+        private void viewQuotesButton_Click(object sender, EventArgs e)
+        {
+            ViewAllQuotes viewQuotesForm = new ViewAllQuotes();
+            viewQuotesForm.Tag = this;
+            viewQuotesForm.Show(this);
+            Hide();
+        }
+
+        private void searchQuotesButton_Click(object sender, EventArgs e)
+        {
+            SearchQuotes searchQuotesForm = new SearchQuotes();
+            searchQuotesForm.Tag = this;
+            searchQuotesForm.Show(this);
+            Hide();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
